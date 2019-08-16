1e47e8dd9f2a53e133cdc638fa3fcf49c2c3304d
Paty Alves <patriciaalves10@gmail.com>
2019-08-16 20:52:35 -0300

    create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projetForm: FormGroup;
+
+  ngOnInit() {
+    this.projetForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectStatus': new FormControl('finished'),
+
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projetForm);
+  }
 }
